26fdb6a2f200a2537e08915531c074d2369cd929
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2022-08-30 01:01:36 +0600

    Work on Test 3

--- Assets/Scripts/Test 3/Orbiter.cs
+++ Assets/Scripts/Test 3/Orbiter.cs
@@ -45,8 +45,13 @@ public class Orbiter : MonoBehaviour
         if(distance > attractor.Radius) return false;
         
         _currentlyRotatingAround = attractor;
-        
+
         // TODO: Implement rest of the function if needed
+
+        //---------Anzamul Haque Akash-----------------------------Start
+        
+        //---------Anzamul Haque Akash-----------------------------End
+
 
         return true;
     }
@@ -54,5 +59,11 @@ public class Orbiter : MonoBehaviour
     void KeepRotating()
     {
         // TODO: Implement the function for rotation
+
+        //---------Anzamul Haque Akash-----------------------------Start
+
+        transform.RotateAround(GameObject.Find("A").GetComponent<Transform>().transform.position, Vector3.up, m_RPMSpeed  * Time.deltaTime);
+
+        //---------Anzamul Haque Akash-----------------------------End
     }
 }
