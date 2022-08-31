d4d270c06dec65bfb6f331be37141bdeed15805a
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2022-09-01 02:30:00 +0600

    Working on test 3

--- Assets/Scripts/Test 4/ChainIK.cs
+++ Assets/Scripts/Test 4/ChainIK.cs
@@ -10,15 +10,33 @@ public class ChainIK : MonoBehaviour
 
     //Anzamul Haque Akash------------------------------------------------------------------------------------------------Start
     [SerializeField] private GameObject[] m_chain; //Here I store chain objects
-    private float m_distanceStartFromGoal; // Distance Start From Goal variable
+    private double m_distanceStartFromGoal; // Distance Start From Goal variable
+    [Range(1,30)]
+    [SerializeField] private int m_topNodespeed;
+    Vector3 m_topNodeLookTargetDirection;
 
-    //Update function
-    private void Update()
+    //Fixed Update function
+    private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0)) //Left mouse button click
         {
             ChainMoveIK();
         }
+        else if(Vector3.Distance(m_chain[0].transform.position, m_chain[6].transform.position) > 12f) //Set start node to top node in 12.. distance if goal distance > 12.. from start
+        {
+            m_topNodeLookTargetDirection = (m_chain[0].transform.position - m_chain[6].transform.position).normalized;
+            m_chain[6].transform.Translate(m_topNodeLookTargetDirection * Time.deltaTime * m_topNodespeed);
+        }
+
+
+
+        //If start to goal distance lesser then total chain distance this function is call
+        if (Vector3.Distance(m_chain[0].transform.position, m_Target.transform.position) < 12f && Vector3.Distance(m_chain[6].transform.position, m_Target.transform.position) > 0.4f)
+        {
+            DistanceLesserThenTotalChainDistance();
+        }
+        //##########################################################################################
+
     }
     //================
     private void ChainMoveIK() //Chain Move function
@@ -29,20 +47,26 @@ public class ChainIK : MonoBehaviour
         {
             DistanceBiggerThenTotalChainDistance();
         }
-        else //start to goal distance lesser then total chain distance 
-        {
-            DistanceLesserThenTotalChainDistance();
-        }
     }
     //================
     private void DistanceBiggerThenTotalChainDistance() //If start to goal distance bigger then total chain distance this function is call
     {
-        m_chain[0].transform.LookAt(m_Target); //Root Node jast use LookAt(Buildin) function
+        float m_stopDistance = Vector3.Distance(m_chain[0].transform.position, m_Target.transform.position) - 12f;
+
+        if (Vector3.Distance(m_chain[6].transform.position, m_Target.transform.position) > m_stopDistance){
+
+            m_topNodeLookTargetDirection = (m_Target.position - m_chain[6].transform.position).normalized;
+            m_chain[6].transform.Translate(m_topNodeLookTargetDirection * Time.deltaTime * m_topNodespeed);
+        }
     }
+    //================
+
     //================
     private void DistanceLesserThenTotalChainDistance() //If start to goal distance lesser then total chain distance this function is call
     {
-
+        m_topNodeLookTargetDirection = (m_Target.position - m_chain[6].transform.position).normalized;
+        m_chain[6].transform.Translate(m_topNodeLookTargetDirection * Time.deltaTime * m_topNodespeed);
     }
+    //================
     //Anzamul Haque Akash------------------------------------------------------------------------------------------------End
 }
