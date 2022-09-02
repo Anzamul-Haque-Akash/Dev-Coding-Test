a3efff1b0f85140aa1f87c23cd26cc8b556770fe
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2022-09-03 01:54:15 +0600

    Almost done test 3

--- Assets/Scripts/Test 4/ChainIK.cs
+++ Assets/Scripts/Test 4/ChainIK.cs
@@ -8,12 +8,11 @@ public class ChainIK : MonoBehaviour
 
     //Anzamul Haque Akash------------------------------------------------------------------------------------------------Start
     [SerializeField] private GameObject[] m_chain; //Here I store chain objects
-    [Range(1, 20)]
-    [SerializeField] private int m_topNodespeed; //Speed Range value(top node)
 
     //Fixed Update function
     private void FixedUpdate()
     {
+        ChildNodeUpdatePosition(); //Child Nodes update position function call
 
         //If start to goal distance < then total chain distance this function is call
         if (Vector3.Distance(m_chain[0].transform.position, m_Target.position) < 12f)
@@ -21,31 +20,25 @@ public class ChainIK : MonoBehaviour
             DistanceLesserThenTotalChainDistance();
         }
         //If start to goal distance > then total chain distance this function is call
-        if (Vector3.Distance(m_chain[0].transform.position, m_Target.position) > 12f && Vector3.Distance(m_chain[0].transform.position, m_Target.position) < 25f)
+        if (Vector3.Distance(m_chain[0].transform.position, m_Target.position) > 12f)
         {
-            m_chain[6].transform.LookAt(m_Target.position); //Always top node look at the target node.
-
             if (Input.GetMouseButton(0))
             {
-
                 DistanceBiggerThenTotalChainDistance();
-                
             }
-            else if (Vector3.Distance(m_chain[0].transform.position, m_chain[6].transform.position) > 12f)
+            else if(Vector3.Distance(m_chain[0].transform.position, m_chain[6].transform.position) > 12f)
             {
                 Vector3 velocity = Vector3.zero;
-                m_chain[6].transform.position = Vector3.SmoothDamp(m_chain[6].transform.position, m_chain[0].transform.position, ref velocity, 0.2f);
+                m_chain[6].transform.position = Vector3.SmoothDamp(m_chain[6].transform.position, m_chain[0].transform.position, ref velocity, 0.1f);
             }
-
         }
-        
-
     }
     //================
     private void DistanceBiggerThenTotalChainDistance() //If start to goal distance bigger then total chain distance this function is call
     {
+        m_chain[6].transform.LookAt(m_Target.position); //Always top node look at the target node.
         Vector3 velocity = Vector3.zero;
-        m_chain[6].transform.position = Vector3.SmoothDamp(m_chain[6].transform.position, m_Target.position, ref velocity, 0.2f);
+        m_chain[6].transform.position = Vector3.SmoothDamp(m_chain[6].transform.position, m_Target.position, ref velocity, 0.3f);
     }
     //================
 
@@ -53,11 +46,38 @@ public class ChainIK : MonoBehaviour
     private void DistanceLesserThenTotalChainDistance() //If start to goal distance lesser then total chain distance this function is call
     {
         m_chain[6].transform.LookAt(m_Target.position); //Always top node look at the target node.
-
         Vector3 velocity = Vector3.zero;
         m_chain[6].transform.position = Vector3.SmoothDamp(m_chain[6].transform.position, m_Target.position, ref velocity, 0.05f);
     }
     //================
+
+
+
+    public void ChildNodeUpdatePosition() //Child Nodes update position function
+    {
+        for(int i=5; i>0; i--)
+        {
+            m_chain[i].transform.LookAt(m_chain[i+1].transform.position);
+
+            if (Vector3.Distance(m_chain[i].transform.position, m_chain[i+1].transform.position) > 2f) {
+                Vector3 velocity = Vector3.zero;
+                m_chain[i].transform.position = Vector3.SmoothDamp(m_chain[i].transform.position, m_chain[i+1].transform.position, ref velocity, 0.06f);
+            }
+
+        }
+
+        for (int i=1; i < 6 ; i++)
+        {
+            if (Vector3.Distance(m_chain[i].transform.position, m_chain[i - 1].transform.position) > 2f)
+            {
+                Vector3 velocity = Vector3.zero;
+                m_chain[i].transform.position = Vector3.SmoothDamp(m_chain[i].transform.position, m_chain[i - 1].transform.position, ref velocity, 0.06f);
+            }
+
+        }
+
+        m_chain[0].transform.LookAt(m_chain[1].transform.position);
+    }
     //Anzamul Haque Akash------------------------------------------------------------------------------------------------End
 
 }
